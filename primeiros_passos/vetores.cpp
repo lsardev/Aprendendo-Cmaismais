aecec02a8953302b993c78810a8f539fd2ed0545
lsardev <devandfinancelsar@gmail.com>
2023-07-28 19:38:28 -0300

    update 1.2

---
+++ primeiros_passos/vetores.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int vetor[5];
+
+    vetor[0]=10;
+    vetor[1]=20;
+    vetor[2]=30;
+    vetor[3]=40;
+    vetor[4]=50;
+
+    cout << vetor[0];
+
+
+    return 0;
+}
---
+++ primeiros_passos/vetores3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int tam=5;
+    int vetor[tam]={10,20,30,40,50};
+    int i;
+
+
+    for(i=0; i<tam; i++){
+        cout << vetor[i] << "\n";
+    }
+
+
+    return 0;
+}
